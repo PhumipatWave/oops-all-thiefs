039cb7cf6b378a63fa57b5b6dbcb16987cf44f29
PhumipatSawasdee <phumipat2345@gmail.com>
2026-02-10 10:53:45 +0700

    Update & Change project setup
    # Re-change assembly name
    + Add some new scripts

--- Assets/OppsAllThiefs/Scripts/UI/LobbyUIControl.cs
+++ Assets/OppsAllThiefs/Scripts/UI/LobbyUIControl.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class IDamageable : MonoBehaviour
+public class LobbyUIControl : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
--- Assets/OppsAllThiefs/Scripts/UI/MenuUIControl.cs
+++ Assets/OppsAllThiefs/Scripts/UI/MenuUIControl.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class MenuController : MonoBehaviour
+public class MenuUIControl : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
