cab71b9d6b3f4dfc00781f2c607ac0795393711a
ET331 <nthwrrssamarth@gmail.com>
2026-02-22 23:32:28 +0700

    Add Item

--- Assets/OppsAllThiefs/Scripts/Tests/TestMoneyUI.cs
+++ Assets/OppsAllThiefs/Scripts/Tests/TestMoneyUI.cs
@@ -1,8 +1,13 @@
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
+using TMPro;
+
 
 public class TestMoneyUI : CharacterStatHandle
 {
+    public float money;
+    
+    
     
 }
