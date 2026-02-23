3457b46322f3e5b0377b9e27291a22d04534e9ad
supashy23-sketch <supashy23@gmail.com>
2026-02-23 20:27:54 +0700

    Add turning direction
    Add turning direction based on walking direction.

--- Assets/OppsAllThiefs/Scripts/Tests/TestPlayer.cs
+++ Assets/OppsAllThiefs/Scripts/Tests/TestPlayer.cs
@@ -3,32 +3,94 @@ using UnityEngine.InputSystem;
 
 public class TestPlayer : MonoBehaviour
 {
-    private Rigidbody rb;
-    private Vector2 moveInput;
-
+    [Header("Movement Settings")]
     [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float rotationSpeed = 10f;
+
+    private Rigidbody rb;
+    private Animator anim;
+
+    private Vector2 moveInput;
+    private InputAction moveAction;
+    private InputAction attackAction;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+
+        moveAction = new InputAction("Move", InputActionType.Value);
+        moveAction.AddCompositeBinding("2DVector")
+            .With("Up", "<Keyboard>/w")
+            .With("Down", "<Keyboard>/s")
+            .With("Left", "<Keyboard>/a")
+            .With("Right", "<Keyboard>/d");
+
+        attackAction = new InputAction("Attack", InputActionType.Button, "<Mouse>/leftButton");
     }
 
-    public void OnMove(InputValue value)
+    private void OnEnable()
     {
-        moveInput = value.Get<Vector2>();
+        moveAction.Enable();
+        attackAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        moveAction.Disable();
+        attackAction.Disable();
+    }
+
+    private void Update()
+    {
+        moveInput = moveAction.ReadValue<Vector2>();
+
+        if (attackAction.triggered)
+        {
+            Attack();
+        }
     }
 
     private void FixedUpdate()
     {
-        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
-        rb.linearVelocity = move * moveSpeed;
+        Move();
+        Rotate();
     }
 
-    public void OnAttack(InputValue value)
+    private void Move()
     {
-        if (value.isPressed)
+        Vector3 moveDirection = new Vector3(moveInput.x, 0f, moveInput.y);
+
+        Vector3 velocity = moveDirection * moveSpeed;
+        velocity.y = rb.linearVelocity.y;
+
+        rb.linearVelocity = velocity;
+    }
+
+    private void Rotate()
+    {
+        Vector3 moveDirection = new Vector3(moveInput.x, 0f, moveInput.y);
+
+        if (moveDirection.sqrMagnitude > 0.01f)
         {
-            Debug.Log("Attack!");
+            Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+            Quaternion smoothRotation = Quaternion.Slerp(
+                rb.rotation,
+                targetRotation,
+                rotationSpeed * Time.fixedDeltaTime
+            );
+
+            rb.MoveRotation(smoothRotation);
+        }
+    }
+
+    private void Attack()
+    {
+        Debug.Log("Attack!");
+
+        if (anim != null)
+        {
+            anim.SetTrigger("Attack");
         }
     }
 }
