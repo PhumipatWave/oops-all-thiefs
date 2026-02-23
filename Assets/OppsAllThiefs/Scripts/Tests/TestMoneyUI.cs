54ee5235e603e3361a11bc1ea6e201b5a5657992
ET331 <nthwrrssamarth@gmail.com>
2026-02-23 15:45:17 +0700

    Add Money

--- Assets/OppsAllThiefs/Scripts/Tests/TestMoneyUI.cs
+++ Assets/OppsAllThiefs/Scripts/Tests/TestMoneyUI.cs
@@ -6,8 +6,37 @@ using TMPro;
 
 public class TestMoneyUI : CharacterStatHandle
 {
-    public float money;
+    public TextMeshProUGUI moneyText;
+    private int money = 0;
     
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            AddItem();
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            SubtractItem();
+        }
+    }
     
+    public void AddItem()
+    {
+        money += 100;
+        UpdateUI();
+    }
+
+    public void SubtractItem()
+    {
+        if (money > 0) money -= 100;
+        UpdateUI();
+    }
     
+    void UpdateUI()
+    {
+        if (moneyText != null)
+            moneyText.text = $"Money: ${money}";
+    }
 }
