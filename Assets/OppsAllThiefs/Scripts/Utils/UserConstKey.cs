5f2391fa1fd2444164e3131f15b257e451a248c9
Phumipat Sawasdee <phumipat2345@gmail.com>
2026-02-25 01:20:14 +0700

    Base lobby system
    + Add lobby with rellay system
    (But now it not completely)
    * Issue now lobby system can't use password to join in private lobby
    * Issue not synce lobby name

---
+++ Assets/OppsAllThiefs/Scripts/Utils/UserConstKey.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public static class UserConstKey
+{
+    public const string playerNameKey = "PlayerName";
+
+    public static string GetPlayerNameKey()
+    {
+        return playerNameKey;
+    }
+}
